e453a38970c0ac8e50f8b2820998a7e62284d0a2
seh0106 <seh0106@auburn.edu>
2024-03-08 09:42:18 -0600

    created preliminary body for gen_report_data -- untested until other methods are added.

--- reportutils.py
+++ reportutils.py
@@ -1,13 +1,46 @@
-
+from PIL import Image
+import PIL
 
 def gen_report_text():
-    pass
+   pass
+
 
 def gen_report_images():
     pass
 
-def gen_report_data():
-    pass
+# takes the dataframe as the parameter in order to split the data by pitcher and later
+# by pitch type
+def gen_report_data(df):
+    # loops through each unique pitcher
+    names = df['Pitcher'].unique()
+    for name in names:
+        # gets (general) text output and saves to text file corresponding to their name
+        text_output = gen_report_text()
+        f = open(name + ".txt", "w")
+        f.write(text_output)
+        f.close()
+        # gets (general) image output and saves to jpg file corresponding to their name
+        image_output = gen_report_images()
+        im1 = Image.open(image_output)
+        im1 = im1.save(name + ".jpg")
+        # loops through all the kinds of pitches for that person
+        pitch_type = df[('TaggedPitchType')].unique()
+        for pitches in pitch_type:
+            # gets pitch specific text output and saves to text file corresponding to their name and pitch
+            text_output_pitch = gen_report_text()
+            f = open(name + pitches + ".txt", "w")
+            f.write(text_output_pitch)
+            f.close()
+            # gets pitch specific image output and saves to text file corresponding to their name and pitch
+            image_output_pitch = gen_report_images()
+            im1 = Image.open(image_output_pitch)
+            im1.save(name + ".jpg")
+            im1.close()
+    # these two lines below i am unsure of...
+    # what is this function supposed to return? There will be too many images/text files to reference?
+    latex_output = report_to_latex()
+    return latex_output
+
 
 def report_to_latex():
     pass
