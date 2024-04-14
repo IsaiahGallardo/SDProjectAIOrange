d9e7a0af633e34c73812ac71130137525130df5e
IsaiahGallardo <isaiahegallardo@gmail.com>
2024-04-14 00:41:26 -0500

    optimized heatmap code with numpy

--- heatmap.py
+++ heatmap.py
@@ -1,17 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def normalize(heatmap: list):
-    """
-    Normalizes a heatmap.
 
-    heatmap: the heatmap to normalize
-
-    return: a 2D list of values representing the normalized heatmap
-    """
-    max_value = max([max(row) for row in heatmap])
-    return [[value / max_value for value in row] for row in heatmap]
-
-def heatmap(values, res_x, res_y, spr=0.25, range_x=None, range_y=None):
+def heatmap(values, res_x, res_y, spr=0.5, range_x=None, range_y=None):
     """
     Creates a heatmap from a list of values.
 
@@ -19,7 +10,7 @@ def heatmap(values, res_x, res_y, spr=0.25, range_x=None, range_y=None):
         are the coordinates of the value and w is the weight of the value.
     res_x: resolution of the x axis
     res_y: resolution of the y axis
-    spr: the amount of spread to the neighbouring pixels. if not specified, it is set to 0.25.
+    spr: the amount of spread to the neighbouring pixels. if not specified, it is set to 0.5.
     range_x: the range of the x inputs (min, max) to include in the heatmap.
         If the x value is outside of this range, it is ignored. If not specified,
         the range is set to the min and max of the data
@@ -27,7 +18,7 @@ def heatmap(values, res_x, res_y, spr=0.25, range_x=None, range_y=None):
         If the y value is outside of this range, it is ignored. If not specified,
         the range is set to the min and max of the data
 
-    return: a 2D list of values representing the heatmap
+    return: a res_x by res_y np.ndarray representing the heatmap
     """
 
     # if the range is not specified, it is set to the min and max of the data
@@ -36,20 +27,21 @@ def heatmap(values, res_x, res_y, spr=0.25, range_x=None, range_y=None):
     if range_y is None:
         range_y = (min(values, key=lambda x: x[1])[1], max(values, key=lambda x: x[1])[1])
     
-    heatmap = [[0 for i in range(res_x)] for j in range(res_y)]
-    offsets = [(0,0,1), (0,1,spr), (1,1,spr), (1,0,spr), (1,-1,spr), (0,-1,spr), (-1,-1,spr), (-1,0,spr), (-1,1,spr)]
+    heatmap = np.zeros((res_y, res_x))
+    scale_x = res_x / (range_x[1] - range_x[0])
+    scale_y = res_y / (range_y[1] - range_y[0])
     for x, y, w in values:
         if range_x[0] <= x <= range_x[1] and range_y[0] <= y <= range_y[1]:
-            x = int((x - range_x[0]) / (range_x[1] - range_x[0]) * res_x)
-            y = int((y - range_y[0]) / (range_y[1] - range_y[0]) * res_y)
-            for offset in offsets:
-                x2 = x + offset[0]
-                y2 = y + offset[1]
-                if 0 <= x2 < res_x and 0 <= y2 < res_y:
-                    heatmap[y2][x2] += w*offset[2]
+            heatmap[int((y - range_y[0]) * scale_y)][int((x - range_x[0]) * scale_x)] += w
+    
+    # blurs the heatmap using a 5x5 gaussian kernel
+    kernel = np.array([spr/6, spr/3, 1-spr, spr/3, spr/6])
+    heatmap = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, heatmap)
+    heatmap = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 1, heatmap)
+
     return heatmap
 
-def div_heatmap(h1: list, h2: list):
+def div_heatmap(h1: np.ndarray, h2: np.ndarray):
     """
     Divides two heatmaps.
 
@@ -58,23 +50,30 @@ def div_heatmap(h1: list, h2: list):
 
     return: a 2D list of values representing the quotient heatmap
     """
-    if len(h1) != len(h2) or len(h1[0]) != len(h2[0]):
-        raise ValueError("Heatmaps are not the same size")
-    heatmap = [[0 for i in range(len(h1[0]))] for j in range(len(h1))]
-    for i in range(len(h1)):
-        for j in range(len(h1[0])):
-            if h2[i][j] != 0:
-                heatmap[i][j] = h1[i][j] / h2[i][j]
-    return heatmap
+    if h1.shape != h2.shape:
+        raise ValueError("Heatmaps must be the same shape")
+    return h1 / (h2 + 1e-9)
+
+def normalize(heatmap: np.ndarray):
+    """
+    Normalizes a heatmap.
+
+    heatmap: the heatmap to normalize
+
+    return: a np.ndarray representing the normalized heatmap
+    """
+    return heatmap / np.max(heatmap)
 
 def save_to_image(heatmap: list, filename: str, fig_title: str, ext: tuple):
     """
     Saves a heatmap to an image file.
+    https://matplotlib.org/stable/users/explain/colors/colormaps.html -- here are all the color maps!
 
     heatmap: the heatmap to save
     filename: the name of the file to save the heatmap to
     """
 
-    plt.imshow(heatmap, cmap='hot', interpolation='nearest', origin='lower', extent=ext)
+    plt.imshow(heatmap, cmap='Reds', interpolation='nearest', origin='lower', extent=ext)
     plt.title(fig_title)
     plt.savefig(filename + '.png', bbox_inches='tight')
+
